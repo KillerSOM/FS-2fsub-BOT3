074808891cabc30d3014d50ee6a59deca37d416e
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-03-07 21:59:56 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -5,6 +5,7 @@ from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
@@ -15,7 +16,14 @@ async def stats(bot: Bot, message: Message):
 
 @Bot.on_message(filters.command('bcmd') & filters.user(ADMINS))
 async def bcmd(bot: Bot, message: Message):
-    tmp_msg = await message.reply("<b>❏ Cᴏᴍᴍᴀɴᴅs ғᴏʀ ʙᴏᴛ Aᴅᴍɪɴs\n\n‣ /start :</b> start the bot or get posts\n<b>‣ /batch :</b> create group messages\n<b>‣ /genlink :</b> create link for one post\n<b>‣ /users :</b> view bot statistics\n<b>‣ /broadcast :</b> broadcast Message\n<b>‣ /stats :</b> checking your bot uptime\n")
+    reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("⛔️ Close", callback_data = "close")
+         ]
+               #[ InlineKeyboardButton("🤖 Bot Commands", callback_data = "command"), InlineKeyboardButton("⛔️ Close", callback_data = "close")]
+         ])
+    tmp_msg = await message.reply(text="<b>❏ Cᴏᴍᴍᴀɴᴅs ғᴏʀ ʙᴏᴛ Aᴅᴍɪɴs\n\n‣ /start :</b> start the bot or get posts\n<b>‣ /batch :</b> create group messages\n<b>‣ /genlink :</b> create link for one post\n<b>‣ /users :</b> view bot statistics\n<b>‣ /broadcast :</b> broadcast Message\n<b>‣ /stats :</b> checking your bot uptime\n", reply_markup = reply_markup)
     await asyncio.sleep(30)
     await tmp_msg.delete()
     await message.delete()
