79e8d798f9ef58708b0b9491a0aea350f1918991
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-03-08 12:59:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped..")
+                self.LOGGER(__name__).info("Bot Stopped..")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL1:
@@ -59,7 +59,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped..")
+                self.LOGGER(__name__).info("Bot Stopped..")
                 sys.exit()
                 
         try:
@@ -70,12 +70,12 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped..")
+            self.LOGGER(__name__).info("Bot Stopped..")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
         self.LOGGER(__name__).info(f"Bot Running..!")
-        self.LOGGER(__name__).info(f""" \n🇩 🇴 🇳 🇪  ✅""")
+        self.LOGGER(__name__).info(f"""🇩 🇴 🇳 🇪  ✅""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
