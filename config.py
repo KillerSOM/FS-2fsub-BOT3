c5ad93caf365d2db69d01ba2017fea5556f34be8
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-03-14 20:10:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -52,7 +52,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>⚠️ Hᴇʏ, {mention} ×\n\nJoin my Channels first to receive files or initiate message\n\n❗Facing problems, use: /help</b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>⚠️ Hᴇʏ, {mention} ×\n\nJoin my Channels first to receive files or initiate message\n\n❕Facing problems, use: /help</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
