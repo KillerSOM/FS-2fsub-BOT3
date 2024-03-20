65116d12889a6ad33e16fe3ecce4caa6ecef65ae
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-03-20 10:20:56 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,62 +1,53 @@
 #(©)Codexbotz
 
 from bot import Bot
+from config import CHNL_MSG
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+SCP = "https://graph.org//file/97dba257afa602043b070.jpg"
+SOLO = "https://graph.org//file/e50b4e50421ddaaab858f.jpg"
+BTTH = "https://graph.org//file/b07335e8e0e5353cbc784.jpg"
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "about":
-        await query.message.edit_text(
-                        text = f"<b>About this Bot:\n\n  A Telegram Bot for storing posts or files that can be accessed via a Special Link.\n\n👨‍💻 Modified by @Shidoteshika1</b>",
-            disable_web_page_preview = True,
+    if data == "chnl1":
+        await query.message.edit_photo(
+                        photo = BTTH,
+                        caption = f"<b>○ Channel: <a href ='https://t.me/BTTH480P'>Battle Through The Heavens</a>\n\n○ Chat Group: <a href ='https://t.me/Yan_Alliance'>Yan Alliance</a>\n\n○ Owner: @Shidoteshika1</b>",
+            #disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ', url = 'https://t.me/Animemoviesr'),
-                        InlineKeyboardButton('Oɴɢᴏɪɴɢ Aɴɪᴍᴇ', url = 'https://t.me/Infinity_Ongoing')
-                    ],[
-                        InlineKeyboardButton("⌬ My Owner ⌬", url = "https://t.me/Shidoteshika1")
-                    ],[
-                        InlineKeyboardButton("⬅️", callback_data = "start"),
-                        InlineKeyboardButton("⛔️", callback_data = "close")
+                        InlineKeyboardButton("⬅️ Back", callback_data = "start"),
+                        InlineKeyboardButton("⛔️ Close", callback_data = "close")
                     ]])
         )
-    elif data == "help":
-           await query.message.edit_text(
-                        text = """<b>○ <u>BOT COMMANDS</u> ○
-
-❏ Cᴏᴍᴍᴀɴᴅs ғᴏʀ ʙᴏᴛ Aᴅᴍɪɴs
-
-‣ /start :</b> start the bot or get posts
-<b>‣ /batch :</b> create group messages
-<b>‣ /genlink :</b> create link for one post
-<b>‣ /users :</b> view bot statistics
-<b>‣ /broadcast :</b> broadcast Message
-<b>‣ /stats :</b> checking your bot uptime
-
-➪ For more Help Contact- <b>@ChatBox480</b>""",
-            disable_web_page_preview = True,
+    elif data == "chnl2":
+           await query.message.edit_photo(
+                        photo = SOLO,
+                        caption = f"<b>○ Channel: <a href ='https://t.me/Solo_Leveling_EngSubb'>Solo Leveling</a>\n\n○ Chat Group: <a href ='https://t.me/Leveling_Group'>Leveling Box</a>\n\n○ Owner: @Shidoteshika1</b>",
+            #disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🤖", callback_data = "about"),
-                        InlineKeyboardButton("⛔️", callback_data = "close"),
-                        InlineKeyboardButton("⬅️", callback_data = "start")
+                        InlineKeyboardButton("⬅️ Back", callback_data = "start"),
+                        InlineKeyboardButton("⛔️ Close", callback_data = "close")
                     ]])
         )
     elif data == "start":
-        await query.message.edit_text(
-                    text = """<b>Hᴇʟʟᴏ !!\n\nI ᴀᴍ Oɴʟʏ Sᴛᴏʀᴇ ғɪʟᴇs ғᴏʀ <a href='https://t.me/Animemoviesr'>infinity void ∞</a></b>""",
-            disable_web_page_preview = True,
+        await query.message.edit_photo(
+                    photo = SCP,
+                    text = CHNL_MSG,
+            #disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
             [
+                
+                    #InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️', url='https://t.me/animemoviesr/3171')
                 [
-                    InlineKeyboardButton('⛩️ OUR OTHER CHANNELS ⛩️', url='https://t.me/animemoviesr/3171')
-                ], [
-                     InlineKeyboardButton("🤖", callback_data = "about"),
-                     InlineKeyboardButton("⛔️", callback_data = "close"),
-                     InlineKeyboardButton("❕", callback_data = "help")
+                     InlineKeyboardButton("Cʜᴀɴɴᴇʟ 𝟷", callback_data = "chnl1"),
+                     InlineKeyboardButton("Cʜᴀɴɴᴇʟ 𝟸", callback_data = "chnl2"),
+                     #InlineKeyboardButton("❕", callback_data = "help")
                 ]])
         )
     elif data == "command":
