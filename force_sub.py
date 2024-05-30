f688997752c8ccac3621602d694f764ff30fdfd7
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-05-30 11:18:04 +0530

    Update force_sub.py

--- force_sub.py
+++ force_sub.py
@@ -15,6 +15,10 @@ from pyrogram import Client, filters
 from database.database import add_channel, del_channel, get_all_channels
 FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1 = 0, 0
 
+@Bot.on_message(filters.command('test') & filters.private & filters.user(OWNER_ID))
+async def test_msg(client:Client, message:Message):
+    await message.reply('Yup working fine: Test, Owner...//')
+
 
 @Bot.on_message(filters.command('add_fsub') & filters.private & filters.user(OWNER_ID))
 async def add_forcesub(client:Client, message:Message):
