c6454063f135f770b14431d4f7e9bee11f1d5950
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-05-30 16:49:37 +0530

    Update force_sub.py

--- plugins/force_sub.py
+++ plugins/force_sub.py
@@ -10,13 +10,14 @@ from database.database import add_channel, del_channel, get_all_channels
 
 @Bot.on_message(filters.command('add_fsub') & filters.private & filters.user(OWNER_ID))
 async def add_forcesub(client:Client, message:Message):
+    pro = await message.reply("<b><i>Processing....</i></b>")
     check=0
     channel_ids = await get_all_channels()
     fsubs = message.text.split()[1:]
     
     #if not (len(fsubs)==1 and fsubs[0].isdigit() and fsubs[0]=='0'):
     if len(fsubs) !=2:
-        await message.reply("<b>You need to add 2 Channel ID at a time</b>")
+        await pro.edit("<b>You need to add 2 Channel ID at a time</b>")
         return
     
     for id in fsubs:
@@ -25,7 +26,7 @@ async def add_forcesub(client:Client, message:Message):
         #elif id == '0':
             #check+=2
         else:
-            await message.reply("<b>Invalid channel ID format.</b>")
+            await pro.edit("<b>Invalid channel ID format.</b>")
             return
     
     if check==2:
@@ -35,36 +36,38 @@ async def add_forcesub(client:Client, message:Message):
         for id in fsubs:
             await add_channel(int(id))
     
-    await message.reply(f'<b>Force-Sub Channel Added ✅</b>\n<code><blockquote>{" ".join(fsubs)}</blockquote></code>')
+    await pro.edit(f'<b>Force-Sub Channel Added ✅</b>\n<code><blockquote>{" ".join(fsubs)}</blockquote></code>')
     #await update_fsub(1)
 
 
 
 @Bot.on_message(filters.command('delall_fsub') & filters.private & filters.user(OWNER_ID))
 async def delete_all_forcesub(client:Client, message:Message):
+    pro = await message.reply("<b><i>Processing....</i></b>")
     channels = await get_all_channels()
     #global FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1
   
     if channels:
         for id in channels:
             await del_channel(id)
-        await message.reply("<b><blockquote>⛔️ All Available Channel ID are Deleted...</blockquote></b>")
+        await pro.edit("<b><blockquote>⛔️ All Available Channel ID are Deleted...</blockquote></b>")
         #FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL1 = 0, 0
         #await update_fsub(0)
     else:
-        await message.reply("<b><blockquote>⁉️ No Channel ID Available to Delete !</blockquote></b>")
+        await pro.edit("<b><blockquote>⁉️ No Channel ID Available to Delete !</blockquote></b>")
       
 
 
 
 @Bot.on_message(filters.command('fsub_chnl_ids') & filters.private & filters.user(OWNER_ID))
 async def get_forcesub(client:Client, message: Message):
+    pro = await message.reply("<b><i>Processing....</i></b>")
     channels = await get_all_channels()
     
     if channels:
-        channel_list = "\n".join([f"<code>{channel}</code>" for channel in channels])
+        channel_list = "\n".join([f"{channel}" for channel in channels])
     else:
         channel_list = "❌ No force-sub channels found."
     
-    await message.reply(f"<b><u>📢 FORCE-SUB CHANNEL IDs:</u></b>\n\n<blockquote>{channel_list}</blockquote>")
+    await pro.edit(f"<b><u>📢 FORCE-SUB CHANNEL IDs:</u></b>\n\n<blockquote><code>{channel_list}</code></blockquote>")
    
