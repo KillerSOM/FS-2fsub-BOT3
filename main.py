90e52adb1cf97ba0dcb31b9179b2ad57502324b0
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-05-30 11:44:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+#from bot import Bot
+
+#Bot().run()
 from bot import Bot
 
-Bot().run()
+if __name__ == "__main__":
+    bot = Bot()
+
+    # Register force sub command handlers
+    from force_sub import add_forcesub, delete_all_forcesub, get_forcesub
+    bot.add_handler(add_forcesub)
+    bot.add_handler(delete_all_forcesub)
+    bot.add_handler(get_forcesub)
+
+    bot.run()
+
