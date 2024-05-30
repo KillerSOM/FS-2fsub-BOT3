b273ba3c77792384e42f3ba3349bb3a3ab7ae63b
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-05-30 11:54:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,12 @@
 #Bot().run()
 # main.py
 
+# main.py
+
 from pyrogram import Client
 from bot import Bot
 
 if __name__ == "__main__":
     bot = Bot()
 
-    # Register force sub command handlers
-    from force_sub import add_forcesub, delete_all_forcesub, get_forcesub
-    bot.add_handler(add_forcesub)
-    bot.add_handler(delete_all_forcesub)
-    bot.add_handler(get_forcesub)
-
     bot.run()
-
