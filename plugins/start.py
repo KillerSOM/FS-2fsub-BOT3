6ab9cf8c5fa5b66b6d5eb0065279764c86302584
Sid <155956430+sidbot2024@users.noreply.github.com>
2024-06-01 13:59:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -108,7 +108,7 @@ async def start_command(client: Client, message: Message):
             if DISABLE_CHANNEL_BUTTON:
                 reply_markup = msg.reply_markup
             else:
-                reply_markup = None
+                reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("Join Channel", url="https://t.me/btth480p")]])
 
             try:
                 await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
